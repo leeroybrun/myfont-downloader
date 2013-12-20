38387c410ffd18109c3b380f2a664d8ee4cf2fa5
Leeroy Brun <leeroy.brun@batiplus.ch>
2013-12-20 16:09:56 +0100

    Start dev of direct convert of fonts

--- downloader.js
+++ downloader.js
@@ -1,9 +1,11 @@
 var request = require('request');
 var cli = require('commander');
 var async = require('async');
+var targz = require('tar.gz');
 var pkg = require('./package.json');
 
 var fs = require('fs');
+var path = require('path');
 
 cli
 	.version(pkg.version)
@@ -101,16 +103,90 @@ async.waterfall([
 		console.log('Start download of fonts files...');
 
 		async.eachSeries(files, function(file, cb) {
-			console.log('Downloading "'+file.name+'".');
-			request({
-				method: 'GET',
-				uri: file.url,
-				jar: cookies
-			}).pipe(fs.createWriteStream('fonts/'+file.name+'.'+fontFormat));
+			async.waterfall([
+				function downloadWoff(cbf) {
+					console.log('Downloading "'+file.name+'" in WOFF format.');
 
-			setTimeout(function() {
-				cb(null);
-			}, delayBetweenCalls);
+					var woffFile = 'fonts/woff/'+file.name+'.'+fontFormat;
+					request({
+						method: 'GET',
+						uri: file.url,
+						jar: cookies
+					}).pipe(fs.createWriteStream(woffFile));
+
+					cbf(null, woffFile);
+				},
+
+				function send2Converter(woffFile, cbf) {
+					// Convert font to TTF (https://www.mashape.com/warting/online-font-converter)
+					console.log('Converting "'+file.name+'" to TTF format.');
+
+					var r = request({
+						method: 'POST',
+						url: 'https://ofc.p.mashape.com/directConvert/',//http://requestb.in/p426qzp4
+						headers: {
+					        'X-Mashape-Authorization': 'ugMVmnJS66Ge32taP76XTBnKKOxJ3PLR'
+					    },
+					    encoding: null
+					}, function (error, response, targzFileBuffer) {
+						console.log(error);
+						console.log(response);
+						console.log(targzFileBuffer);
+						cbf(null, targzFileBuffer);
+					});
+
+					var form = r.form();
+					form.append('format', 'ttf');
+					form.append('file', fs.createReadStream(path.join(__dirname, woffFile)));
+
+					console.log(path.join(__dirname, woffFile));
+
+					/*var Request = unirest.post('https://ofc.p.mashape.com/directConvert/')
+						.headers({ 
+							'X-Mashape-Authorization': 'ugMVmnJS66Ge32taP76XTBnKKOxJ3PLR'
+						})
+						.field('format', 'ttf')
+						.attach('file', '../../'+)
+						.end(function (response) {
+							console.log(response.body);
+							cbf(null, response.response.body);
+						});*/
+				},
+
+				function saveTarGz(body, cbf) {
+					console.log('Save tar.gz for "'+file.name+'" font.');
+					var tarGzFile = 'fonts/tmp/'+file.name+'.tar.gz';
+					fs.writeFile(tarGzFile, body, function (err) {
+						if (err) throw err;
+
+						cbf(null, tarGzFile);
+					});
+				},
+
+				function extractTarGz(tarGzFile, cbf) {
+					console.log('Extract tar.gz for "'+file.name+'" font.');
+					var extractedFolder = './fonts/tmp/'+file.name;
+					var compress = new targz().extract(tarGzFile, extractedFolder, function(err){
+						if (err) throw err;
+
+						cbf(null, extractedFolder);
+					});
+				},
+
+				function moveTTF(extractedFolder, cbf) {
+					console.log('Move TTF for "'+file.name+'" font.');
+					var ttfFile = './fonts/ttf/'+file.name+'.ttf';
+					fs.rename(extractedFolder+'/onlinefontconverter.com/converted-files'+file.name+'.ttf', ttfFile, function(err) {
+						if (err) throw err;
+
+						cbf(null, ttfFile);
+					});
+				}
+			], function(err) {
+				setTimeout(function() {
+					cb(null);
+				}, delayBetweenCalls);
+			});			
 		}, function(err) {
 			console.log('Done.');
 			callback(err);
